31b2107b2488eab8bb80c84d2f7ee9bf6e166ff2
Dai <59753705+dialogueeeeee@users.noreply.github.com>
2025-11-02 23:34:17 +0800

    Update install-python-runtime.sh

--- runtime/python/install-python-runtime.sh
+++ runtime/python/install-python-runtime.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
-set -o errexit
-
-USE_OFFICIAL_SOURCE=1
-
 conda create -n sandbox-runtime -y python=3.10
 
-source activate sandbox-runtime
+conda activate sandbox-runtime
 
 pip install -r ./requirements.txt --ignore-requires-python
 
