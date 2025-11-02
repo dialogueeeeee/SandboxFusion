da4122b68e6b9619332a2ea66c7df9a2c0c0229f
Dai <59753705+dialogueeeeee@users.noreply.github.com>
2025-11-02 11:00:18 +0800

    Update install_env.sh to include package installations
    Added installation commands for various Python packages.

---
+++ sandbox/install_env.sh
@@ -0,0 +1,11 @@
+curl -sSL https://install.python-poetry.org | python3 -
+
+pip install structlog
+pip install fastapi
+pip install databases
+pip install pyyaml
+pip install psutil
+pip install tenacity
+pip install aiofiles
+pip install aiosqlite
+pip install transformers
